db19b0bee3171ad99668142f581ffe235fdd82d6
Rohit Anand <94343474+rohit777anand@users.noreply.github.com>
2023-04-30 20:41:18 +0530

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -35,17 +35,18 @@ for row in result:
     global encrypted_data
     sensitive_data = str(row[0])
     encrypted_data = f.encrypt(sensitive_data.encode())
-    cursor.execute("UPDATE rohn2 SET roll=%s WHERE roll=%s", (encrypted_data, sensitive_data))
+    cursor.execute("UPDATE rohn2 SET roll=%s WHERE roll=%s", 
+                   (encrypted_data, sensitive_data))
 
 db.commit()
 print("Data encrypted and updated successfully")
 
 # after running above scripts :-
 # mysql> select * from rohn2;
-# +-------+------------------------------------------------------------------------------------------------------+
-# | name  | roll                                                                                                 |
-# +-------+------------------------------------------------------------------------------------------------------+
-# | sohan | gAAAAABkTlvBmIjqYLvgRWr-1ku4K_1ptUgllZZuu8Eiaq00e74JT7_PA-ZXqkhvFB26bAdLiaTAuooYd82I1vmra0G2Pn_BHw== |
-# | johan | gAAAAABkTlvBflR2Tr15N-hWIQK_0cdqa7yyjwM1donV4NWkzGLaeewoZKPGkz1DtNRxeS5khBnGI9kMeyIcDFO44mEdUzpiQw== |
-# +-------+------------------------------------------------------------------------------------------------------+
+# +-------+-------------------+
+# | name  | roll              |
+# +-------+-------------------+
+# | sohan | gAAAAABkTl==      |
+# | johan | gKPGkz1DdUzpiQw== |
+# +-------+-------------------+
 # 2 rows in set (0.00 sec)
